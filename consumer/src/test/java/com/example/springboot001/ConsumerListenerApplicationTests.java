ae31e7b7a7223c1f0d7fce62eeef7fa8aa908244
Mudong Guo <1198033143@qq.com>
2023-12-15 14:02:42 +0200

    final delivery

--- consumer/src/test/java/com/example/springboot001/ConsumerListenerApplicationTests.java
+++ consumer/src/test/java/com/example/springboot001/ConsumerListenerApplicationTests.java
@@ -1,13 +1,13 @@
-package com.mikrosoft.consumer;
+package com.example.springboot001;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConsumerApplicationTests {
+class ConsumerListenerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
